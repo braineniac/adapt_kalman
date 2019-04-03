ef774c8e02e43219b17b92c08c41b336d86376f4
Daniel Hammer <braineniac@protonmail.com>
2019-04-03 17:08:18 +0200

    Achieved singularity

--- bin/simple_kalman_node.py
+++ bin/simple_kalman_node.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env Python
+#!/usr/bin/env python
 
 import rospy
+import time
 from geometry_msgs.msg import TwistWithCovarianceStamped
 from sensor_msgs.msg import Imu
 from nav_msgs.msg import Odometry
@@ -18,31 +19,34 @@ class SimpleKalmanNode:
         self.newstate = np.zeros(3)
 
         # error covariance
-        self.P = np.zeros(3,3)
-        self.newp = np.zeros(3,3)
+        self.P = np.zeros((3,3))
+        self.newp = np.zeros((3,3))
         # contol vector u
         self.u = np.zeros(3)
 
         # state transition model
-        self.F = np.zeros(3,3)
+        self.F = np.zeros((3,3))
 
         # control-input model
-        self.B = np.zeros(3,3)
+        self.B = np.zeros((3,3))
 
 
         # process noise covariances
-        self.Q = np.zeros(3,3)
+        self.Q = np.zeros((3,3))
         # observation noise covariance
-        self.R = np.zeros(3,3)
+        self.R = np.zeros((3,3))
 
-        self.H = np.zeros(1,3)
+        self.H = np.zeros((1,3))
         self.z = np.zeros(3)
-        self.S = np.zeros(3,3)
+        self.S = np.zeros((3,3))
         self.K = np.zeros(3)
-        self.y k= np.zeros(3)
+        self.y = np.zeros(3)
+        self.y_k = np.zeros(3)
+        self.newP = np.zeros((3,3))
+        self.tosec = rospy.Time.from_sec(time.time()).to_sec()
 
         # process noise
-        self.w = np.zeros(3,3)
+        self.w = np.zeros((3,3))
 
         self.mean = np.zeros(3)
 
@@ -54,42 +58,63 @@ class SimpleKalmanNode:
         self.fake_enc_sub = rospy.Subscriber("/fake_wheel/twist_var", TwistWithCovarianceStamped, self.fake_enc_cb)
         self.odom_pub = rospy.Publisher("/simple_kalman/odom", Odometry, queue_size=1)
 
-        self.publish()
-
         self.begin_time = rospy.Time(0.0)
         self.last_time = rospy.Time(0.0)
-        self.delta_t = rospy.Time(0.0)
+        self.delta_t = 0.0
+
+        self.initialize()
 
     def initialize(self):
         self.begin_time = rospy.get_rostime()
         self.R[1][1] = (400/1000000) * 9.80655 / 100.0  # fake encoder covariance, Xdot
         self.R[2][2] = (400/1000000) * 9.80655          # imu covariance Xdotdot
+        self.R[1][1] = 100
+        self.R[2][2] = 1
         #self.w = np.random.mulivariate_normal(mean,q,1000)
         self.F = np.array([[1,self.delta_t,1/2*self.delta_t*self.delta_t],[0,1,self.delta_t],[0,0,1.0]])
         self.B = np.array([[1,self.delta_t,1/2*self.delta_t*self.delta_t],[0,1,self.delta_t],[0,0,1.0]])
-        self.H = np.array([1.0 0 0])
+        self.H = np.array([1.0, 0, 0])
 
     def predict(self):
         """Kalman filter predict step
         """
         self.state = self.F.dot(self.state) + self.B.dot(self.u)
-        self.newp = self.F.dot(self.p).dot(np.transpose(self.F))
+        self.P = self.F.dot(self.P).dot(np.transpose(self.F))
+
+    def debug_print(self):
+        #np.set_printoptions(precision=15)
+        rospy.loginfo("State: {}".format(self.state))
+        rospy.loginfo("Covariance: {}".format(self.P))
+        rospy.loginfo("Observer cov: {}".format(self.R))
+        rospy.loginfo("Residual: {}".format(self.y))
+        rospy.loginfo("Residual covariance:".format(self.S))
+        rospy.loginfo("Kalman gain: {}".format(self.K))
+        rospy.loginfo("New state: {}".format(self.newstate))
+        rospy.loginfo("New covariance: {}".format(self.newP))
+        rospy.loginfo("New residual: {}".format(self.y_k))
 
     def update(self):
         """Kalman filter update step
         """
-        # true state residual
         self.y = self.state - self.H.dot(self.newstate)
+        self.S = np.zeros((3,3))
+        self.debug_print()
         self.S = self.R + self.H.dot(self.P).dot(np.transpose(self.H))
+        self.debug_print()
         self.K = self.P.dot(np.transpose(self.H)).dot(np.linalg.inv(self.S))
         self.newstate = self.state + self.K.dot(self.y)
         self.newP = (np.identity(3) - self.K.dot(self.H)).dot(self.P).dot(np.transpose(np.identity(3) - self.K.dot(self.H))) + self.K.dot(self.R).dot(np.transpose(self.K))
         self.y_k = self.state - self.H.dot(self.newstate)
 
+        # update for the next cycle
+        self.publish()
+        self.P = self.newP
+        self.state = self.newstate
+
     def imu_cb(self, imu_msg):
         # update time
         now = rospy.get_rostime()
-        self.delta_t = self.last_time - now
+        self.delta_t = self.tosec(self.last_time - now)
         self.last_time = now
 
         self.u[2] = imu_msg.linear_acceleration.x
@@ -97,7 +122,7 @@ class SimpleKalmanNode:
     def fake_enc_cb(self):
         # update time
         now = rospy.get_rostime()
-        self.delta_t = self.last_time - now
+        self.delta_t = self.tosec(self.last_time - now)
         self.last_time = now
 
         self.u[1] = self.twist.twist.linear.x
@@ -112,4 +137,6 @@ if __name__ == '__main__':
     simple_kalman_node = SimpleKalmanNode()
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
+        simple_kalman_node.predict()
+        simple_kalman_node.update()
         rate.sleep()
