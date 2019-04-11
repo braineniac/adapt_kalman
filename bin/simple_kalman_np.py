cdf69d140880bb59c66f9d6fed67b086407f02e0
Daniel Hammer <braineniac@protonmail.com>
2019-04-11 13:54:17 +0200

    Filter working

--- bin/simple_kalman_np.py
+++ bin/simple_kalman_np.py
@@ -4,8 +4,20 @@ from matplotlib import pyplot as plt
 class SimpleKalman:
     def __init__(self):
 
+        self.small_val = np.exp(-20)
+        self.delta_t = 0.0
+
+        self.sum_t = 0.0
+        self.plot_t = []
+        self.plot_y = []
+        self.plot_v = []
+        self.plot_a = []
+
+        self.u = [[],[]]
+        self.t = []
+
         self.L_k = np.zeros((2,2))          # Kalman gain
-        self.P_k_pre = np.random.normal(np.exp(-20),1.0,(2,2))      # A priori covariance
+        self.P_k_pre = np.random.normal(self.small_val,1.0,(2,2))      # A priori covariance
         self.P_k_post = np.zeros((2,2))     # A posteriori covariance
         self.C_k = np.zeros(2)
         self.x_k_post = np.zeros((2,1))
@@ -16,50 +28,44 @@ class SimpleKalman:
         self.D_k = np.zeros((2,2))
         self.gamma_k = np.zeros((2,2))      # control matrix
         self.R_k = np.zeros((2,2))
-        #self.Q_k = np.random.normal(np.exp(-10),1.0,(3,3))
+        self.Q_k = np.zeros((2,2))
         self.G_k = np.zeros((2,2))
         self.H_k = np.zeros((2,2))
         self.y_k = np.zeros((2,1))          # output
         self.u_k = np.zeros((2,1))          # control vector
 
-        self.u = [[],[]]
-        self.t = []
-
-        self.delta_t = 0.0
-
         # set initial covariance
         #imu_stdev = (400/1000000) * 9.80655
         #fake_enc_stdev = (400/1000000) * 9.80655 / 100.0
         imu_stdev = 0.5
-        fake_enc_stdev = 0.0001
-        self.R_k[0][0] = fake_enc_stdev*fake_enc_stdev
+        fake_enc_stdev = 0.01
+        self.R_k[0][0] = self.small_val
         self.R_k[1][1] = imu_stdev*imu_stdev
-        #self.v_k = np.array([[fake_enc_stdev],[imu_stdev]])
+        self.Q_k[0][0] = self.small_val
+        self.Q_k[1][1] = fake_enc_stdev*fake_enc_stdev
 
-        self.plot_t = []
-        self.plot_y = []
-        self.plot_v = []
-        self.sum_t = 0.0
+        self.H_k[1][1] = 1
+        self.G_k[1][1] = 1
 
     def time_update(self):
-        self.D_k = np.array([[self.delta_t,1/2*self.delta_t*self.delta_t],[1,self.delta_t]])
-        self.phi_k = np.array([[1,self.delta_t],[0,1]])
-        self.gamma_k = np.array([[self.delta_t, 1/2*self.delta_t*self.delta_t],[1,self.delta_t]])
-        self.C_k = np.array([[1,self.delta_t],[0,1]])
+        self.D_k = np.array([[0,0],[0,self.delta_t]])
+        self.phi_k = np.array([[1,self.delta_t],[0,0]])
+        self.gamma_k = np.array([[0,0],[0,1]])
+        self.C_k = np.array([[0,0],[1/2*self.delta_t*self.delta_t,self.delta_t]])
 
     def set_gain(self):
-        E = self.C_k.dot(self.P_k_pre).dot(self.C_k.T) + self.R_k
+        E = self.C_k.dot(self.P_k_pre).dot(self.C_k.T) + self.H_k.dot(self.Q_k).dot(self.H_k.T) + self.R_k
         self.L_k = self.P_k_pre.dot(self.C_k.T).dot(np.linalg.inv(E))
 
     def update(self):
         F = self.y_k - self.C_k.dot(self.x_k_pre) - self.D_k.dot(self.u_k)
         self.x_k_post = self.x_k_pre + self.L_k.dot(F)
         self.P_k_post = (np.identity(2) - self.L_k.dot(self.C_k)).dot(self.P_k_pre)
-        self.y_k = self.C_k.dot(self.x_k_post)
+        #self.y_k = self.C_k.dot(self.x_k_post)
 
     def extrapolate(self):
         self.x_k_extr = self.phi_k.dot(self.x_k_post) + self.gamma_k.dot(self.u_k)
-        self.P_k_extr = self.phi_k.dot(self.P_k_post).dot(self.phi_k.T)
+        self.P_k_extr = self.phi_k.dot(self.P_k_post).dot(self.phi_k.T) + self.G_k.dot(self.Q_k).dot(self.G_k.T)
 
         # update for next iteration
         self.x_k_pre = self.x_k_extr
@@ -80,32 +86,42 @@ class SimpleKalman:
                 self.t.append(t[1])
 
     def kalman_filter(self):
-        for u0,u1,t in zip(np.diff(np.array(self.u[0])),np.diff(np.array(self.u[1])),np.diff(np.array(self.t))):
+        for u0,u1,t in zip(np.array(self.u[0]),np.array(self.u[1]),np.diff(np.array(self.t))):
             self.delta_t = t
-            self.u_k[0] = u0
-            self.u_k[1] = -u1
+            self.u_k[0] = 0
+            self.u_k[1] = u0
+            self.y_k[0] = 0
+            self.y_k[1] = -u1
             self.time_update()
             self.set_gain()
             self.update()
             self.extrapolate()
             self.sum_t = self.sum_t + t
-            self.plot_y.append(self.y_k[0])
-            self.plot_v.append(self.y_k[1])
+            self.plot_y.append(self.x_k_pre[0])
+            self.plot_v.append(self.x_k_pre[1])
+            self.plot_a.append(-u1)
             self.plot_t.append(self.sum_t)
         self.print_debug()
         self.plot_output()
 
     def plot_output(self):
-        plt.subplot(2,1,1)
-        plt.title("Kalman filter straight line then stop")
+        plt.subplot(131)
+        plt.title("Kalman filter x state")
         plt.ylabel("Distance travelled in m")
         plt.xlabel("Time in sec")
         plt.plot(self.plot_t, self.plot_y)
 
-        plt.subplot(2,1,2)
+        plt.subplot(132)
+        plt.title("Kalman filter ẋ state")
         plt.xlabel("Time in sec")
         plt.ylabel("Velocity in m/s")
         plt.plot(self.plot_t,self.plot_v)
+
+        plt.subplot(133)
+        plt.title("Imu data")
+        plt.xlabel("Time in sec")
+        plt.ylabel("Acceleration in m/s^2")
+        plt.plot(self.plot_t,self.plot_a)
         plt.show()
 
     def print_debug(self):
