6efee3b7c4f0090b8f4b3b00cc93b3c3c4439b29
Daniel Hammer <braineniac@protonmail.com>
2019-05-26 10:21:12 +0200

    reading rosbag working

--- bin/simple_kalman_bag.py
+++ bin/simple_kalman_bag.py
@@ -6,58 +6,70 @@ from scipy import signal
 from matplotlib import pyplot as plt
 import rosbag
 import argparse
+from simple_kalman import SimpleKalman
 
 class SimpleKalmanBag:
-    def __init__(self,bagpath=None):
-        self.bag_path = bagpath
+    def __init__(self,bag_path=None):
+        self.bag_path = bag_path
+        self.bag = rosbag.Bag(bag_path)
+        self.kalman = SimpleKalman(1000,5)
 
         self.u = [[],[]]
         self.t = []
-        self.input = []
+        self.imu = []
+        self.t_imu = []
+        self.twist = []
+        self.t_twist = []
 
-    def read_bag(self):
-        bag = rosbag.Bag(self.bag_path)
-        imu_msgs = bag.read_messages(topics=['/imu/data_raw'])
-        twist_msgs = bag.read_messages(topics=['/fake_wheel/twist'])
-        last_vel = 0.0
-        last_accel = 0.0
-        for imu_msg,twist_msg in zip(imu_msgs,twist_msgs):
+    def read_imu(self, topic):
+        msgs = self.bag.read_messages(topics=topic)
+        for imu_msg in msgs:
             imu_t = imu_msg.message.header.stamp.to_sec()
-            twist_t = twist_msg.message.header.stamp.to_sec()
             imu_a_x = imu_msg.message.linear_acceleration.x
-            twist_l_x = twist_msg.message.twist.twist.linear.x
-            if last_vel != twist_l_x:
-                self.input.append((twist_t,twist_l_x,last_accel))
-                last_vel = twist_l_x
-            if last_accel != imu_a_x:
-                self.input.append((imu_t,last_vel,imu_a_x))
-                last_accel = imu_a_x
-        self.sort2np()
+            self.imu.append((imu_t,imu_a_x))
 
-    def sort2np(self):
-        sorted_input = sorted(self.input,key=lambda elem : elem[0])
-        for t,u0,u1 in sorted_input:
-            self.u[0].append(u0)
-            self.u[1].append(u1)
-            self.t.append(t)
+    def read_twist(self,topic):
+        msgs = self.bag.read_messages(topics=topic)
+        for twist_msg in msgs:
+            twist_t = twist_msg.message.header.stamp.to_sec()
+            twist_l_x = twist_msg.message.twist.twist.linear.x
+            self.twist.append((twist_t,twist_l_x))
+
+    def upscale_twist(self):
+        last_j = (0,0)
+        for _j in self.twist:
+            t_twist,x_twist = _j
+            last_twist_t, last_x_twist = last_j
+            for _k in self.imu:
+                t_imu,x_imu = _k
+                if last_twist_t <= t_imu and t_twist > t_imu:
+                    self.u[0].append(last_x_twist)
+                    self.u[1].append(x_imu)
+                    self.t.append(t_imu)
+                elif t_twist < t_imu:
+                    break;
+            last_j = _j
+
+    def run_filter(self):
+        for u,t in zip(zip(self.u[0],self.u[1]), np.diff(self.t)):
+            self.kalman.filter(u,t)
 
     def plot_all(self):
-        plt.figure(1)
-        print(self.u)
-        print(self.t)
-        print(self.input)
+        plt.figure(2)
 
-        plt.subplot(121)
+        plt.subplot(211)
         plt.title("Imu data")
         plt.xlabel("Time in s")
         plt.ylabel("Acceleration in m/s^2")
-        plt.plot(self.t,self.u[1])
+        plt.plot(self.t, self.u[1])
 
-        plt.subplot(122)
+        plt.subplot(212)
         plt.title("Fake wheel encoder")
         plt.xlabel("Time in s")
         plt.ylabel("Velocity")
-        plt.plot(self.t,self.u[0])
+        plt.plot(self.t, self.u[0])
+
+        self.kalman.plot_all()
 
         plt.show()
 
@@ -66,5 +78,8 @@ if __name__ == '__main__':
     parser.add_argument("bagpath",help="Rosbag path")
     args = parser.parse_args()
     simple_kalman_bag = SimpleKalmanBag(args.bagpath)
-    simple_kalman_bag.read_bag()
+    simple_kalman_bag.read_imu("/imu")
+    simple_kalman_bag.read_twist("/fake_wheel/twist")
+    simple_kalman_bag.upscale_twist()
+    simple_kalman_bag.run_filter()
     simple_kalman_bag.plot_all()
--- bin/simple_kalman_sim.py
+++ bin/simple_kalman_sim.py
@@ -7,35 +7,27 @@ from simple_kalman import SimpleKalman
 
 class SimpleKalmanSim:
 
-    def __init__(self, N=1000, sim_time=10.0,sigma=0.1, peak_vel=1.14):
-        t = np.linspace(0,sim_time,N)
-        x = np.linspace(-sim_time/2.0,sim_time/2.0,N)
-        self.t = t
-        self.x = x
-        self.vel = np.piecewise(t, [t<0.1*sim_time,t>1,t>0.9*sim_time], [0,peak_vel,0])
-        self.gauss = np.exp(-(x/sigma)**2/2)
-        self.conv = None
-        self.accel = None
-        self.kalman = SimpleKalman(100,5)
+    def __init__(self, N=1000, sim_time=5.0, peak_vel=1.14):
+        self.t = np.linspace(0,sim_time,N)
+        self.vel = self.set_vel(sim_time,peak_vel,N)
+        self.accel = self.set_accel(sim_time,N)
+        self.kalman = SimpleKalman(1000,5)
+
+    def set_vel(self,sim_time,peak_vel, N):
+        t = self.t
+        box_function = np.piecewise(t, [t<0.1*sim_time,t>1,t>0.9*sim_time], [0,peak_vel,0])
+        return box_function
 
     def plot_all(self):
-        plt.figure(1)
+        plt.figure(2)
 
-        plt.subplot(221)
+        plt.subplot(211)
         plt.title("Simulated robot velocity")
         plt.xlabel("Time in s")
         plt.ylabel("Velocity in m/s")
         plt.plot(self.t,self.vel)
 
-        plt.subplot(222)
-        plt.title("Gauss used for the convolution")
-        plt.plot(self.x, self.gauss)
-
-        plt.subplot(223)
-        plt.title("Result of the convolution")
-        plt.plot(self.t,self.conv)
-
-        plt.subplot(224)
+        plt.subplot(212)
         plt.title("Simulated robot acceleration")
         plt.xlabel("Time in s")
         plt.ylabel("Acceleration in m/s^2")
@@ -43,17 +35,22 @@ class SimpleKalmanSim:
 
         plt.show()
 
-    def simulate_input(self):
-        self.conv = np.convolve(self.vel,self.gauss/self.gauss.sum(), mode="same")
-        self.accel = 100*np.gradient(self.conv)
-        #self.plot_all()
+    def set_accel(self,sim_time,N):
+        sigma = 0.01
+        x = np.linspace(-sim_time/2.0,sim_time/2.0,N)
+        gauss = np.exp(-(x/sigma)**2/2)
+        conv = np.convolve(self.vel,gauss/gauss.sum(), mode="same")
+        grad = 100*np.gradient(conv)
+        noise = np.random.normal(0,6,N)
+        offset = 1.5
+        return grad + noise + offset
 
     def run_filter(self):
         for u,t in zip(zip(self.vel,self.accel), np.diff(self.t)):
             self.kalman.filter(u,t)
         self.kalman.plot_all()
+        self.plot_all()
 
 if __name__ == '__main__':
     simple_kalman_sim = SimpleKalmanSim()
-    simple_kalman_sim.simulate_input()
     simple_kalman_sim.run_filter()
