32a6bf8531176c15dbfe8a808797fabff7bdbab7
Daniel Hammer <braineniac@protonmail.com>
2019-06-12 11:51:18 +0200

    Intial work on adjustable covars

--- bin/simple_kalman.py
+++ bin/simple_kalman.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import matplotlib
+#matplotlib.use('Qt5Agg',warn=False, force=True)
 from matplotlib import pyplot as plt
+
+import pandas as pd
+from fractions import Fraction
 
 class SimpleKalman:
     def __init__(self, u0_stdev=0.01,u1_stdev=0.01):
@@ -24,7 +29,13 @@ class SimpleKalman:
         self.plot_y = []
         self.plot_v_post = []
         self.plot_v_pre = []
+        self.plot_u0 = []
         self.plot_a = []
+
+        self.ratio = u0_stdev/u1_stdev
+        self.coeff_array = []
+        self.last_array = []
+        self.peak = 0
 
         self.L_k = np.zeros((2,2))                                  # Kalman gain
         self.P_k_pre = np.random.normal(small_val,1.0,(2,2))        # A priori covariance
@@ -82,6 +93,8 @@ class SimpleKalman:
         self.u_k[1] = u[0]
         self.y_k[0] = 0
         self.y_k[1] = u[1]
+
+        self.adapt_covar(20)
         self.time_update()
         self.set_gain()
         self.update()
@@ -90,8 +103,81 @@ class SimpleKalman:
         self.sum_t = self.sum_t + t
         self.plot_y.append(self.x_k_post[0])
         self.plot_v_post.append(self.x_k_post[1])
+        self.plot_u0 = np.append(self.plot_u0, [u[0]])
         self.plot_a = np.append(self.plot_a,[u[1]])
         self.plot_t.append(self.sum_t)
+
+    def get_abs_array(self, array = []):
+        if array == []:
+            return None
+
+        array_pos = []
+        for x in array:
+            if x < 0:
+                x = -x
+            array_pos.append(x)
+        return array_pos
+
+    def get_diff_sum(self, array = [], N=0):
+        if array == []:
+            return None
+
+        np_array = np.array(array).reshape((1,N))[0]
+        print(np_array)
+        #array_diff = np.diff(np_array)
+        #print(array_diff)
+        #array_pos = self.get_abs_array(array_diff)
+        #print(array_pos)
+        array_sum = np.sum(np_array)
+        if array_sum < 1.0e-10:
+            array_sum = 1.0e-10
+
+        return array_sum
+
+    def get_current_N(self, N):
+        array = []
+        for i in range(0,N):
+            array.append(self.plot_u0[-i-1])
+            print(self.plot_u0[-i-1])
+        return array
+
+    def adapt_covar(self, N=0):
+        if len(self.plot_u0) == N:
+            self.last_array = self.get_current_N(N)
+
+        elif len(self.plot_u0) >= 2*N:
+            current_array = self.get_current_N(N)
+            last_array = self.last_array
+            last_diff_sum = self.get_diff_sum(last_array, N)
+            current_diff_sum = self.get_diff_sum(current_array,N)
+
+            last_series = pd.Series(last_array)
+            current_series = pd.Series(current_array)
+            peak = np.amax(current_array)
+            if peak > self.peak:
+                self.peak = peak
+
+            current_avg = np.average(current_series.ewm(span=N).mean().values)
+            last_avg = np.average(last_series.ewm(span=N).mean().values)
+
+            coeff = np.log(current_avg/last_avg)
+
+            self.coeff_array.append(coeff)
+
+            new_ratio = Fraction(self.ratio * coeff).limit_denominator(200)
+
+            u0_stdev = new_ratio.numerator
+            u1_stdev = new_ratio.denominator
+            print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+            print("coeff: {}".format(coeff))
+            print("u0 stdev: {}".format(u0_stdev))
+            print("u1 stdev: {}".format(u1_stdev))
+            print("last diff sum: {}".format(last_diff_sum))
+            print("current diff sum: {}".format(current_diff_sum))
+            self.Q_k[1][1] = u0_stdev*u0_stdev
+            self.R_k[1][1] = u1_stdev*u1_stdev
+
+            self.last_array = current_array
 
     def plot_all(self):
         plt.figure(1)
--- bin/simple_kalman_sim.py
+++ bin/simple_kalman_sim.py
@@ -22,28 +22,63 @@ class SimpleKalmanSim:
 
     def plot_all(self):
         plt.figure(1)
-        #plt.title("Simulated robot velocity")
+
+        plt.subplot(511)
+        plt.title("Simulated robot velocity")
+        plt.xlabel("Time in s")
+        plt.ylabel("Velocity in m/s")
+        plt.plot(self.t,self.vel)
+
+        # plt.subplot(512)
+        # plt.title("Simulated robot acceleration")
+        # plt.xlabel("Time in s")
+        # plt.ylabel("Acceleration in m/s^2")
+        # plt.plot(self.t,self.accel)
+
+        plt.subplot(512)
+        plt.title("Robot distance")
+        plt.xlabel("Time in s")
+        plt.ylabel("Distance in m")
+        plt.plot(self.kalman.plot_t,self.kalman.plot_y)
+
+        plt.subplot(513)
+        plt.title("Robot velocity post")
+        plt.xlabel("Time in s")
+        plt.ylabel("Velocity in m/s")
+        plt.plot(self.kalman.plot_t,self.kalman.plot_v_post)
+
+        plt.subplot(514)
+        plt.title("Robot acceleration")
+        plt.xlabel("Time in s")
+        plt.ylabel("Acceleration in m/s^2")
+        plt.plot(self.kalman.plot_t, self.kalman.plot_a)
+
+        plt.subplot(515)
+        plt.title("Coeff change")
+        plt.plot(self.kalman.coeff_array)
+
+        plt.show()
+
+    def export_plots(self):
+        plt.figure(1)
         plt.xlabel("Time in s")
         plt.ylabel("Velocity in m/s")
         plt.plot(self.t,self.vel)
         matplotlib2tikz.save("plots/input_vel_sim.tex",figureheight='4cm', figurewidth='6cm')
 
         plt.figure(2)
-        #plt.title("Simulated robot acceleration")
         plt.xlabel("Time in s")
         plt.ylabel("Acceleration in m/s^2")
         plt.plot(self.t,self.accel)
         matplotlib2tikz.save("plots/input_accel_sim.tex",figureheight='4cm', figurewidth='6cm' )
 
         plt.figure(3)
-        #plt.title("Robot distance")
         plt.xlabel("Time in s")
         plt.ylabel("Distance in m")
         plt.plot(self.kalman.plot_t,self.kalman.plot_y)
         matplotlib2tikz.save("plots/robot_dist_sim.tex",figureheight='3cm', figurewidth='14cm' )
 
         plt.figure(4)
-        #plt.title("Robot velocity post")
         plt.xlabel("Time in s")
         plt.ylabel("Velocity in m/s")
         plt.plot(self.kalman.plot_t,self.kalman.plot_v_post)
@@ -51,24 +86,41 @@ class SimpleKalmanSim:
         matplotlib2tikz.save("plots/robot_vel_sim.tex",figureheight='3cm', figurewidth='14cm' )
 
         plt.figure(5)
-        #plt.title("Robot acceleration")
         plt.xlabel("Time in s")
         plt.ylabel("Acceleration in m/s^2")
         plt.plot(self.kalman.plot_t, self.kalman.plot_a)
         matplotlib2tikz.save("plots/robot_accel_sim.tex",figureheight='3cm', figurewidth='14cm' )
 
-        #plt.show()
+    def get_noise_moving(self, peak_coeff):
+        noise_moving = []
+        for x in self.vel:
+            # fill staying still with zeros
+            if abs(x) < 0.01:
+                noise_moving.append(0.0)
+            else:
+                noise_moving.append(np.random.normal(0,x*peak_coeff))
+
+        return noise_moving
 
     def set_accel(self,sim_time,N):
+        accel = 0
         sigma = 0.01
         x = np.linspace(-sim_time/2.0,sim_time/2.0,N)
         gauss = np.exp(-(x/sigma)**2/2)
         conv = np.convolve(self.vel,gauss/gauss.sum(), mode="same")
         grad = 50*np.gradient(conv)
         grad_shift = 0.7 * np.roll(grad,10)
-        noise = np.random.normal(0,0.2,N)
+        noise_still = np.random.normal(0,0.03,N)
+        noise_moving = self.get_noise_moving(1.5)
         offset = 0.3
-        return grad + grad_shift + noise + offset
+
+        accel += grad
+        #accel += grad_shift
+        #accel += noise_still
+        #accel += noise_moving
+        #accel += offset
+
+        return accel
 
     def run_filter(self):
         for u,t in zip(zip(self.vel,self.accel), np.diff(self.t)):
