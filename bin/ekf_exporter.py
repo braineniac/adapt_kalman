e2fdacc0d59bc36305ab55e26cb0d5a9eed93f1f
Daniel Hammer <braineniac@protonmail.com>
2019-06-30 08:22:17 +0200

    create exporter for ekf localization

---
+++ bin/ekf_exporter.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import matplotlib2tikz
+import argparse
+from matplotlib import pyplot as plt
+import numpy as np
+
+import rosbag
+
+
+class EKFExporter:
+    def __init__(self, bag_path="", odom_topic="/odometry/filtered"):
+        self.bag_path = bag_path
+        self.odom_topic = odom_topic
+
+        self.vel = []
+        self.pos = []
+        self.t = []
+
+    def read_bag(self):
+        rosbag_f = rosbag.Bag(self.bag_path)
+        odom_msgs = rosbag_f.read_messages(topics=self.odom_topic)
+        for odom_msg in msgs:
+            pos_x = odom_msg.pose.pose.position.x
+            vel_x = odom_msg.twist.twist.x
+            t = odom_msg.header.stamp.to_sec()
+            self.pos.append(pos_x)
+            self.vel.append(vel_x)
+            self.t.append(t)
+
+        self.plot_t = np.abs(self.t - self.t[0])
+
+    def plot_all(self):
+        plt.figure(1)
+
+        plt.subplot(211)
+        plt.title("EKF distance")
+        plt.plot(self.plot_t,self.pos)
+
+        plt.subplot(212)
+        plt.title("EKF velocity")
+        plt.plot(self.plot_t,self.vel)
+
+        plt.show()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Process rosbag of EKF")
+    parser.add_argument("-b","--bag",help="Rosbag path")
+    parser.add_argument("-t", "--topic", help="Topic name")
+    args = parser.parse_args()
+    ekf_exporter = EKFExporter(args.bag,args.topic)
+    ekf_exporter.read_bag()
+    ekf_exporter.plot_all()
