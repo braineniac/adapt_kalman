5bf3d64211c580e1c752f9ccdd60ff5af73e37f5
Daniel Hammer <braineniac@protonmail.com>
2019-08-17 17:12:08 +0200

    Restructure and fix window calculation, add main export file for thesis, check if Q_k and R_k empty

---
+++ bin/moving_weighted_window.py
@@ -0,0 +1,82 @@
+# Copyright (c) 2019 Daniel Hammer. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+import pandas as pd
+
+class MovingWeightedWindow(object):
+
+    def __init__(self,size=0):
+        if size:
+            self._size = size
+            self._weights = []
+            self._window = []
+        else:
+            raise AttributeError
+
+    def get_avg(self):
+        raise NotImplementedError
+
+    def _set_weights(self):
+        raise NotImplementedError
+
+    def set_window(self,array=None):
+        if array:
+            window = []
+            for i in range(self._size):
+                window.append(array[-i-1])
+        self._window = window
+
+    def get_window(self):
+        return self._window
+
+class MovingWeightedExpWindow(MovingWeightedWindow):
+
+    def __init__(self, size, alpha=5.0):
+        if alpha:
+            self._alpha = alpha
+        else:
+            raise AttributeError
+        super(MovingWeightedExpWindow, self).__init__(size)
+
+    def get_avg(self):
+        series = pd.Series(self._window)
+        ewm = series.ewm(span=self._alpha).mean().values
+        return ewm[-1]
+
+class MovingWeightedSigWindow(MovingWeightedWindow):
+
+    def __init__(self,size, alpha=10):
+        super(MovingWeightedSigWindow, self).__init__(size)
+        if alpha:
+            self._alpha = alpha
+            self._set_weights()
+        else:
+            raise AttributeError
+
+    def get_avg(self):
+        y = np.zeros(self._size)
+        for i in range(self._size):
+            upper_sum = 0
+            for k in range(i+1):
+                upper_sum += self._window[k] * self._weights[k]
+            y[i] = upper_sum / np.sum(self._weights[:i+1])
+        return y[-1]
+
+    def _set_weights(self):
+        x = np.linspace(0,1,self._size)
+        w = np.zeros(self._size)
+        for i in range(self._size):
+            w[i] = 1 / (1 + np.exp(self._alpha * (x[i]-0.5)))
+        self._weights = w
