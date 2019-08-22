38f7c394870ab955291f1865eb109840ee6c0a66
Daniel Hammer <braineniac@protonmail.com>
2019-08-22 21:01:45 +0200

    Capitalized matrix names

--- src/adapt_kalman/kalman_filter.py
+++ src/adapt_kalman/kalman_filter.py
@@ -17,48 +17,49 @@ from collections import deque
 
 from moving_weighted_window import MovingWeightedWindow
 
+
 class KalmanFilter(object):
 
-    def __init__(self, Q_k=None, R_k=None, alpha=1.,beta=1.,x0 = [0,0,0,0,0]):
+    def __init__(self, Q_k=None, R_k=None, alpha=1., beta=1., x0=[0, 0, 0, 0, 0]):
         if np.count_nonzero(Q_k) <= 1 or np.count_nonzero(R_k) <= 1:
             raise AttributeError
         else:
             self._alpha = alpha
             self._beta = beta
 
-            self._R_k = R_k                            # observation noise covaraiance matrix
-            self._Q_k = Q_k                            # process noise covariance matrix
+            self._R_k = R_k  # observation noise covaraiance matrix
+            self._Q_k = Q_k  # process noise covariance matrix
 
-            self._u_k = np.zeros((2,1))                # control input vector
-            self._y_k = np.zeros((2,1))                # observation vector
-            self._L_k = np.zeros((5,2))                # Kalman gain matrix
+            self._u_k = np.zeros((2, 1))  # control input vector
+            self._y_k = np.zeros((2, 1))  # observation vector
+            self._L_k = np.zeros((5, 2))  # Kalman gain matrix
 
-            self._x_k_pre = np.zeros((5,1))            # A priori state vector
-            self._x_k_post = np.zeros((5,1))           # A posteriori state vector
-            self._x_k_extr = np.zeros((5,1))           # extrapolated state vector
+            self._X_k_pre = np.zeros((5, 1))  # A priori state vector
+            self._X_k_post = np.zeros((5, 1))  # A posteriori state vector
+            self._X_k_extr = np.zeros((5, 1))  # extrapolated state vector
 
             x0[3] = np.radians(x0[3])
-            self._x_k_pre = np.array(x0).reshape((5,1))
+            self._X_k_pre = np.array(x0).reshape((5, 1))
 
-            self._P_k_pre = np.zeros((5,5))            # A priori covariance matrix
-            self._P_k_post = np.zeros((5,5))           # A posteriori covariance matrix
-            self._P_k_extr = np.zeros((5,5))           # extrapolated covariance matrix
+            self._P_k_pre = np.zeros((5, 5))  # A priori covariance matrix
+            self._P_k_post = np.zeros((5, 5))  # A posteriori covariance matrix
+            self._P_k_extr = np.zeros((5, 5))  # extrapolated covariance matrix
 
-            self._phi_k = np.zeros((5,5))              # dynamics matrix
-            self._gamma_k = np.zeros((5,2))            # control matrix
-            self._G_k = np.zeros((5,2))                #
+            self._Phi_k = np.zeros((5, 5))  # dynamics matrix
+            self._Gamma_k = np.zeros((5, 2))  # control matrix
+            self._G_k = np.zeros((5, 2))  #
             self._G_k[2][0] = self._alpha
             self._G_k[4][1] = self._beta
 
-            self._C_k = np.zeros((2,5))                # measurement matrix
-            self._D_k = np.zeros((2,2))                # measurement input matrix
-            self._H_k = np.zeros((2,2))                #
+            self._C_k = np.zeros((2, 5))  # measurement matrix
+            self._D_k = np.zeros((2, 2))  # measurement input matrix
+            self._H_k = np.zeros((2, 2))  #
 
             self._dt = 0
             self._t = 0
 
-    def filter_iter(self, tuy = (None,None,None)):
-        u,y,t = tuy
+    def filter_iter(self, tuy=(None, None, None)):
+        u, y, t = tuy
         if t is None and u is None and y is None:
             raise ValueError
         else:
@@ -80,34 +81,34 @@ class KalmanFilter(object):
             self._setup_next_iter()
 
     def get_post_states(self):
-        return tuple(self._x_k_post)
+        return tuple(self._X_k_post)
 
     def get_Q(self):
         return (self._Q_k)
 
     def _update_matrices(self):
         if self._dt:
-            self._phi_k = np.array([
-                [1,0,self._dt*np.cos(self._x_k_post[3]),0,0],
-                [0,1,self._dt*np.sin(self._x_k_post[3]),0,0],
-                [0,0,0,0,0],
-                [0,0,0,1,self._dt],
-                [0,0,0,0,0]
+            self._Phi_k = np.array([
+                [1, 0, self._dt * np.cos(self._X_k_post[3]), 0, 0],
+                [0, 1, self._dt * np.sin(self._X_k_post[3]), 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 1, self._dt],
+                [0, 0, 0, 0, 0]
             ])
-            self._gamma_k = np.array([
-                [0,0],
-                [0,0],
-                [self._alpha,0],
-                [0,0],
-                [0,self._beta]
+            self._Gamma_k = np.array([
+                [0, 0],
+                [0, 0],
+                [self._alpha, 0],
+                [0, 0],
+                [0, self._beta]
             ])
             self._C_k = np.array([
-                [0,0,-1/self._dt,0,0],
-                [0,0,0,0,1]
+                [0, 0, -1 / self._dt, 0, 0],
+                [0, 0, 0, 0, 1]
             ])
             self._D_k = np.array([
-                [self._alpha/self._dt,0],
-                [0,0]
+                [self._alpha / self._dt, 0],
+                [0, 0]
             ])
 
     def _set_gain(self):
@@ -115,40 +116,41 @@ class KalmanFilter(object):
         self._L_k = self._P_k_pre.dot(self._C_k.T).dot(np.linalg.inv(E))
 
     def _update_states(self):
-        F = self._y_k - self._C_k.dot(self._x_k_pre) - self._D_k.dot(self._u_k)
-        self._x_k_post = self._x_k_pre + self._L_k.dot(F)
+        F = self._y_k - self._C_k.dot(self._X_k_pre) - self._D_k.dot(self._u_k)
+        self._X_k_post = self._X_k_pre + self._L_k.dot(F)
 
     def _update_error_covars(self):
         self._P_k_post = (np.identity(5) - self._L_k.dot(self._C_k)).dot(self._P_k_pre)
 
     def _extr_states(self):
-        self._x_k_extr = self._phi_k.dot(self._x_k_post) + self._gamma_k.dot(self._u_k)
+        self._X_k_extr = self._Phi_k.dot(self._X_k_post) + self._Gamma_k.dot(self._u_k)
 
     def _extr_error_covars(self):
-        self._P_k_extr = self._phi_k.dot(self._P_k_post).dot(self._phi_k.T) + self._G_k.dot(self._Q_k).dot(self._G_k.T)
+        self._P_k_extr = self._Phi_k.dot(self._P_k_post).dot(self._Phi_k.T) + self._G_k.dot(self._Q_k).dot(self._G_k.T)
 
     def _setup_next_iter(self):
-        self._x_k_pre = self._x_k_extr
+        self._X_k_pre = self._X_k_extr
         self._P_k_pre = self._P_k_extr
+
 
 class AdaptiveKalmanFilter(KalmanFilter):
 
-    def __init__(self,Q_k=None,R_k=None,alpha=1.0,beta=1.0, window=None, M_k=None,x0=[0,0,0,0,0]):
-        if not isinstance(window,MovingWeightedWindow) or np.count_nonzero(M_k)<2:
+    def __init__(self, Q_k=None, R_k=None, alpha=1.0, beta=1.0, window=None, M_k=None, x0=[0, 0, 0, 0, 0]):
+        if not isinstance(window, MovingWeightedWindow) or np.count_nonzero(M_k) < 2:
             raise AttributeError
         else:
-            super(AdaptiveKalmanFilter,self).__init__(alpha=alpha,beta=beta,Q_k=Q_k,R_k=R_k,x0=x0)
+            super(AdaptiveKalmanFilter, self).__init__(alpha=alpha, beta=beta, Q_k=Q_k, R_k=R_k, x0=x0)
             self._window = window
             self._M_k = M_k
             self._Lambda_k = np.identity(2)
             self._Ro_k = self._Q_k.dot(np.linalg.inv(self._R_k))
-            self._u_buffer = deque([],self._window.get_size())
+            self._u_buffer = deque([], self._window.get_size())
 
-    def filter_iter(self, tuy=(None,None,None)):
-        t,u,y = tuy
+    def filter_iter(self, tuy=(None, None, None)):
+        t, u, y = tuy
         self._u_buffer.append(u)
         self._adapt_covar()
-        super(AdaptiveKalmanFilter,self).filter_iter(tuy)
+        super(AdaptiveKalmanFilter, self).filter_iter(tuy)
 
     def _adapt_covar(self):
         if len(self._u_buffer) >= self._window.get_size():
