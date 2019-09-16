c4f69a7f6c9552e4f330d451350cf99517bc8c68
Daniel Hammer <braineniac@protonmail.com>
2019-09-16 19:34:24 +0200

    Add exec dependencies, package change name

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages={'adapt_kalman'},
-    package_dir={'':'src'},
+    packages={'kalman_estimator'},
+    package_dir={'': 'src'},
     )
 
 setup(**setup_args)
