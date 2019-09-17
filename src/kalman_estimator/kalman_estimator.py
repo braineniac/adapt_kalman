56af1d35423ab8f4030b32f196b8c28515ea6a52
Daniel Hammer <braineniac@protonmail.com>
2019-09-17 08:26:15 +0200

    Rename estimator, pep8 cleanup, new model conversion

--- src/kalman_estimator/kalman_estimator.py
+++ src/kalman_estimator/kalman_estimator.py
@@ -20,7 +20,7 @@ from itertools import compress, chain
 import numpy as np
 from scipy import signal
 
-from kalman_filter import KalmanFilter, AdaptiveKalmanFilter
+from kalman_filter import KalmanFilter
 
 
 def check_directory(dir=None):
@@ -109,26 +109,14 @@ class StateEstimator(object):
             self._stamped_input = stamped_input
             self._add_time_from_input()
 
-    # @staticmethod
-    # def _fix_stamped_input(stamped_input=None):
-    #     if not stamped_input:
-    #         raise ValueError
-    #     else:
-    #         new_stamped_input = []
-    #         for stamp, input in stamped_input:
-    #             u0, u1 = input
-    #             u1 = -u1
-    #             new_stamped_input.append((stamp, (u0, u1)))
-    #         return new_stamped_input
-
     def set_u1y1_zero(self):
         new_stamped_input = []
         new_stamped_output = []
         if self._stamped_input and self._stamped_output:
-            for t,u in self._stamped_input:
+            for t, u in self._stamped_input:
                 u0, u1 = u
                 new_stamped_input.append((t, (u0, 0)))
-            for t,y in self._stamped_output:
+            for t, y in self._stamped_output:
                 y0, y1 = y
                 new_stamped_output.append((t, (y0, 0)))
             self._stamped_input = new_stamped_input
@@ -141,35 +129,35 @@ class StateEstimator(object):
             self._stamped_output = stamped_output
             self._add_time_from_output()
 
-    @staticmethod
-    def _v_state_form(state=None):
-        if not state:
-            raise ValueError
-        else:
-            x, y, psi, xdot, ydot, psidot = state
-            v = np.sqrt(xdot * xdot + ydot * ydot)
-            return x, y, v, psi, psidot
+    # @staticmethod
+    # def _v_state_form(state=None):
+    #     if not state:
+    #         raise ValueError
+    #     else:
+    #         x, y, psi, xdot, ydot, psidot = state
+    #         v = np.sqrt(xdot * xdot + ydot * ydot)
+    #         return x, y, v, psi, psidot
+    #
+    # @staticmethod
+    # def _psi_state_limit(state=None):
+    #     if state is None:
+    #         raise ValueError
+    #     else:
+    #         x, y, v, psi, dpsi = state
+    #         k = abs(int(psi / (2 * np.pi)))
+    #         if psi > 2 * np.pi:
+    #             psi -= 2 * np.pi * k
+    #         elif psi < -2 * np.pi * k:
+    #             psi += 2 * np.pi * (k + 1)
+    #         return x, y, v, psi, dpsi
 
-    @staticmethod
-    def _psi_state_limit(state=None):
-        if state is None:
-            raise ValueError
-        else:
-            x, y, v, psi, dpsi = state
-            k = abs(int(psi / (2 * np.pi)))
-            if psi > 2 * np.pi:
-                psi -= 2 * np.pi * k
-            elif psi < -2 * np.pi * k:
-                psi += 2 * np.pi * (k + 1)
-            return x, y, v, psi, dpsi
-
-    @staticmethod
-    def _order_state(state=None):
-        if not state:
-            raise ValueError
-        else:
-            x, y, psi, v, dpsi = state
-            return x, y, v, psi, dpsi
+    # @staticmethod
+    # def _order_state(state=None):
+    #     if not state:
+    #         raise ValueError
+    #     else:
+    #         x, y, psi, v, dpsi = state
+    #         return x, y, v, psi, dpsi
 
     def _add_time_from_output(self):
         for t_y, _y in self._stamped_output:
@@ -180,13 +168,13 @@ class StateEstimator(object):
             self._time.append(t_u)
 
 
-class KalmanStateEstimator(StateEstimator):
+class KalmanEstimator(StateEstimator):
 
     def __init__(self, kalman_filter=None):
         if not isinstance(kalman_filter, KalmanFilter):
             raise ValueError
         else:
-            super(KalmanStateEstimator, self).__init__()
+            super(KalmanEstimator, self).__init__()
             self._kalman_filter = kalman_filter
 
     def get_stamped_states(self):
@@ -238,7 +226,11 @@ class StatePlotter(object):
             self._end_slice = np.inf
             self._input_titles = ["Input u0", "Input u1"]
             self._output_titles = ["Output y0", "Output y1"]
-            self._states_titles = ["x state", "y state", "v state", "phi state", "dphi state"]
+            self._states_titles = [
+                "x state", "y state",
+                "v state", "a state",
+                "phi state", "dphi state", "ddphi_state"
+            ]
 
     def get_input_titles(self):
         return self._input_titles
@@ -252,7 +244,8 @@ class StatePlotter(object):
     def get_input_plot(self):
         plot_u = [[], []]
         plot_u_t = []
-        sliced_stamped_input = self._slice_data(self._state_estimator.get_stamped_input())
+        stamped_input = self._state_estimator.get_stamped_input()
+        sliced_stamped_input = self._slice_data(stamped_input)
         for u_stamped in sliced_stamped_input:
             t, u = u_stamped
             u0, u1 = u
@@ -264,7 +257,8 @@ class StatePlotter(object):
     def get_output_plot(self):
         plot_y = [[], []]
         plot_y_t = []
-        sliced_stamped_output = self._slice_data(self._state_estimator.get_stamped_output())
+        stamped_output = self._state_estimator.get_stamped_output()
+        sliced_stamped_output = self._slice_data(stamped_output)
         for y_stamped in sliced_stamped_output:
             t, y = y_stamped
             y0, y1 = y
@@ -274,29 +268,26 @@ class StatePlotter(object):
         return plot_y_t, plot_y
 
     def get_states_plot(self):
-        plot_states = [[], [], [], [], []]
+        plot_states = [[], [], [], [], [], [], []]
         plot_states_t = []
-        slices_stamped_states = self._slice_data(self._state_estimator.get_stamped_states())
-        for states_stamped in slices_stamped_states:
+        stamped_states = self._state_estimator.get_stamped_states()
+        sliced_stamped_states = self._slice_data(stamped_states)
+        for states_stamped in sliced_stamped_states:
             t, states = states_stamped
-            x0, x1, x2, x3, x4 = states
-            plot_states[0].append(x0)
-            plot_states[1].append(x1)
-            plot_states[2].append(x2)
-            plot_states[3].append(x3)
-            plot_states[4].append(x4)
+            for i in range(len(states)):
+                plot_states[i].append(states[i])
             plot_states_t.append(t)
         return plot_states_t, plot_states
 
     def get_x0x1_plot(self):
         plot_x0_state = []
         plot_x1_state = []
-        slices_stamped_states = self._slice_data(self._state_estimator.get_stamped_states())
+        stamped_states = self._state_estimator.get_stamped_states()
+        slices_stamped_states = self._slice_data(stamped_states)
         for states_stamped in slices_stamped_states:
             t, states = states_stamped
-            x0, x1, x2, x3, x4 = states
-            plot_x0_state.append(x0)
-            plot_x1_state.append(x1)
+            plot_x0_state.append(states[0])
+            plot_x1_state.append(states[1])
         return plot_x0_state, plot_x1_state
 
     def get_Q_plot(self):
@@ -324,43 +315,59 @@ class StatePlotter(object):
         t, y = self.get_output_plot()
         y0, y1 = y
         dir = "../../data/{}".format(pre)
-        np.savetxt("{}/{}y0{}.csv".format(dir, pre, post), np.transpose([t, y0]), header='t y0', comments='# ',
-                   delimiter=' ', newline='\n')
-        np.savetxt("{}/{}y1{}.csv".format(dir, pre, post), np.transpose([t, y1]), header='t y1', comments='# ',
-                   delimiter=' ', newline='\n')
+        np.savetxt("{}/{}y0{}.csv".format(dir, pre, post),
+                   np.transpose([t, y0]), header='t y0',
+                   comments='# ', delimiter=' ', newline='\n')
+        np.savetxt("{}/{}y1{}.csv".format(dir, pre, post),
+                   np.transpose([t, y1]), header='t y1',
+                   comments='# ', delimiter=' ', newline='\n')
 
     def export_input(self, pre="", post=""):
         t, u = self.get_input_plot()
         u0, u1 = u
         dir = "../../data/{}".format(pre)
         check_directory(dir)
-        np.savetxt("{}/{}u0{}.csv".format(dir, pre, post), np.transpose([t, u0]), header='t u0', comments='# ',
-                   delimiter=' ', newline='\n')
-        np.savetxt("{}/{}u1{}.csv".format(dir, pre, post), np.transpose([t, u1]), header='t u1', comments='# ',
-                   delimiter=' ', newline='\n')
+        np.savetxt("{}/{}u0{}.csv".format(dir, pre, post),
+                   np.transpose([t, u0]), header='t u0',
+                   comments='# ', delimiter=' ', newline='\n')
+        np.savetxt("{}/{}u1{}.csv".format(dir, pre, post),
+                   np.transpose([t, u1]), header='t u1',
+                   comments='# ', delimiter=' ', newline='\n')
 
     def export_states(self, pre="", post=""):
         t, x = self.get_states_plot()
-        x0, x1, x2, x3, x4 = x
+        x0, x1, x2, x3, x4, x5, x6 = x
         dir = "../../data/{}".format(pre)
         check_directory(dir)
-        np.savetxt("{}/{}x0{}.csv".format(dir,pre, post), np.transpose([t, x0]), header='t x0', comments='# ',
-                   delimiter=' ', newline='\n')
-        np.savetxt("{}/{}x1{}.csv".format(dir,pre, post), np.transpose([t, x1]), header='t x1', comments='# ',
-                   delimiter=' ', newline='\n')
-        np.savetxt("{}/{}x2{}.csv".format(dir,pre, post), np.transpose([t, x2]), header='t x2', comments='# ',
-                   delimiter=' ', newline='\n')
-        np.savetxt("{}/{}x3{}.csv".format(dir,pre, post), np.transpose([t, x3]), header='t x3', comments='# ',
-                   delimiter=' ', newline='\n')
-        np.savetxt("{}/{}x4{}.csv".format(dir,pre, post), np.transpose([t, x4]), header='t x4', comments='# ',
-                   delimiter=' ', newline='\n')
+        np.savetxt("{}/{}x0{}.csv".format(dir, pre, post),
+                   np.transpose([t, x0]), header='t x0',
+                   comments='# ', delimiter=' ', newline='\n')
+        np.savetxt("{}/{}x1{}.csv".format(dir, pre, post),
+                   np.transpose([t, x1]), header='t x1',
+                   comments='# ', delimiter=' ', newline='\n')
+        np.savetxt("{}/{}x2{}.csv".format(dir, pre, post),
+                   np.transpose([t, x2]), header='t x2',
+                   comments='# ', delimiter=' ', newline='\n')
+        np.savetxt("{}/{}x3{}.csv".format(dir, pre, post),
+                   np.transpose([t, x3]), header='t x3',
+                   comments='# ', delimiter=' ', newline='\n')
+        np.savetxt("{}/{}x4{}.csv".format(dir, pre, post),
+                   np.transpose([t, x4]), header='t x4',
+                   comments='# ', delimiter=' ', newline='\n')
+        np.savetxt("{}/{}x5{}.csv".format(dir, pre, post),
+                   np.transpose([t, x5]), header='t x5',
+                   comments='# ', delimiter=' ', newline='\n')
+        np.savetxt("{}/{}x6{}.csv".format(dir, pre, post),
+                   np.transpose([t, x6]), header='t x6',
+                   comments='# ', delimiter=' ', newline='\n')
 
     def export_x0x1(self, pre="", post=""):
         x0, x1 = self.get_x0x1_plot()
         dir = "../../data/{}".format(pre)
         check_directory(dir)
-        np.savetxt("{}/{}x0x1{}.csv".format(dir,pre, post), np.transpose([x0, x1]), header='x0 x1', comments='# ',
-                   delimiter=' ', newline='\n')
+        np.savetxt("{}/{}x0x1{}.csv".format(dir, pre, post),
+                   np.transpose([x0, x1]), header='x0 x1',
+                   comments='# ', delimiter=' ', newline='\n')
 
     def export_Q(self, pre="", post=""):
         if self.get_Q_plot():
@@ -368,10 +375,12 @@ class StatePlotter(object):
             Q0, Q1 = Q
             dir = "../../data/{}".format(pre)
             check_directory(dir)
-            np.savetxt("{}/{}Q0{}.csv".format(dir,pre, post), np.transpose([t, Q0]), header='t Q0', comments='# ',
-                   delimiter=' ', newline='\n')
-            np.savetxt("{}/{}Q1{}.csv".format(dir,pre, post), np.transpose([t, Q1]), header='t Q1', comments='# ',
-                   delimiter=' ', newline='\n')
+            np.savetxt("{}/{}Q0{}.csv".format(dir, pre, post),
+                       np.transpose([t, Q0]), header='t Q0',
+                       comments='# ', delimiter=' ', newline='\n')
+            np.savetxt("{}/{}Q1{}.csv".format(dir, pre, post),
+                       np.transpose([t, Q1]), header='t Q1',
+                       comments='# ', delimiter=' ', newline='\n')
 
     @staticmethod
     def filter_butter(array, order=5, fc=1 / 50.):
